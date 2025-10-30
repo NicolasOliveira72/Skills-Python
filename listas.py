0321b73deead3a0727cee27590aa2a602b6118b2
NicolasOliveira72 <nicolas1421rocha@gmail.com>
2025-10-30 11:14:03 -0400

    :sparkles: Trabalhando com listas

--- listas.py
+++ listas.py
@@ -1,4 +1,4 @@
-filme = ["A Origem", 2000, 25, 9.4]
+filme = ["A Origem", "Os pecadores"]
 
 line = "="
 print(line*30)
@@ -8,8 +8,22 @@ print(line*30)
 print(len(filme))
 
 print(line*30)
-print(filme.index(25))
+print(filme.index("Os pecadores"))
 
 print(line*30)
-filme.index(25)
+filme.append("O Conde de Monte Cristo")
+filme.append("A Volta ao Mundo em 80 dias")
+print(filme)
 
+print(line*30)
+filme.sort()
+print(filme)
+
+print(line*30)
+copiadalista = filme.copy()
+copiadalista.remove("O Conde de Monte Cristo")
+print(copiadalista)
+
+print(line*30)
+filme.clear()
+print(f"Lista esvaziada: {filme}")
