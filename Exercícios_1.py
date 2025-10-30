cd5471d8da014b8b349d44c77b95fcf83b692996
NicolasOliveira72 <nicolas1421rocha@gmail.com>
2025-10-30 09:45:01 -0400

    :sparkles: Listas

---
+++ Exercícios_1.py
@@ -0,0 +1,15 @@
+# Exercício 1: Qubrando e invertendo uma string:
+
+# texto = "Python é interessante pro mercado"
+# palavras = texto.split() # Vai quebar a o texto em "palavras de string"
+# textoInvertido = "---".join(palavras[::-1]) # Slicing/Fatiamento
+# print(textoInvertido)
+
+
+# Exercício 2: Verificando se a palavra é um palíndromo:
+
+# word = input("Digite a palavra pra verificar se é um palíndromo: ")
+# wordformat = word.lower().replace(" ","")
+# wordInvert = (wordformat[::-1])
+# result = wordInvert == wordformat
+# print(result)
---
+++ listas.py
@@ -0,0 +1,15 @@
+filme = ["A Origem", 2000, 25, 9.4]
+
+line = "="
+print(line*30)
+print(type(filme))
+
+print(line*30)
+print(len(filme))
+
+print(line*30)
+print(filme.index(25))
+
+print(line*30)
+filme.index(25)
+
