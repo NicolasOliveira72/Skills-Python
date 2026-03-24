7a926de081df1f33ed46280f1e2bbde3df229cbc
NicolasOliveira72 <nicolas1421rocha@gmail.com>
2026-03-24 19:49:36 -0400

    :sparkles: Stock editing functionality.

--- Exercises/Sistema de compras/main.py
+++ Exercises/Sistema de compras/main.py
@@ -1,4 +1,4 @@
-import cliente
+import cliente, repositor
 
 def line():
     lines = "="
@@ -8,7 +8,21 @@ line()
 print("SISTEMA DE VENDAS EM PYTHON")
 line()
 
-itens, valor_total = cliente.comprar([], 0)
-cliente.exportar_recibo(itens, valor_total)
+request = int(input("Digite 1 para realizar compras:\nDigite 2 para editar estoque:"))
+
+if int == 1:
+    itens, valor_total = cliente.comprar([], 0)
+    cliente.exportar_recibo(itens, valor_total)
+elif int ==2:
+    request_client = int(input("Escolha uma opção:\n 1 : Consultar.\n 2 : Inserir/Editar.\n 3 : Remover."))
+    if request_client == 1:
+        repositor.read()
+    elif request_client == 2:
+        repositor.create
+    elif request_client == 3:
+        repositor.delete
+    elif request_client == 4:
+        print("Compra encarrada!")
+    print("Resposta inválida!")
 
 line()
--- Exercises/Sistema de compras/repositor.py
+++ Exercises/Sistema de compras/repositor.py
@@ -1,1 +1,28 @@
 from data import estoque
+
+def read():
+    busca = input("Digite o produto para consulta: ").strip().capitalize()
+    if busca in estoque:
+        print("Produto busca dicponível!")
+        print(f"Preço: {estoque[busca]['Preco']} | Quantidade: {estoque[busca]['Quantidade']}")
+    else:
+        print("Produto não cadastrado")
+    
+def create():
+    pedido = input("Digite o produto que deseja inserir: ").strip().capitalize()
+    if pedido in estoque:
+        print("Produto já cadastrado")
+    else:
+        preco = float(input("Digite o preço do produto: "))
+        qtd = int(input("Digite a quantidade do produto: "))
+        #Inserção ou atualização no dicionáqio:
+        estoque[pedido] = {"Preco" : preco, "Quantidade" : qtd}
+        print(f"Pedido {pedido} cadastrado com sucesso!")
+
+def delete():
+    pedido = input("Digite o produto que deseja excluir: ").strip().capitalize()
+    if pedido in estoque:
+        del estoque[pedido]
+    else:
+        print("Produto não cadastraddo!")
+    
