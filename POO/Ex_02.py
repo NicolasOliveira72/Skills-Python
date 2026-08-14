05604eb7defcf6908ae8439d96eca3d73d58389c
NicolasOliveira72 <nicolas1421rocha@gmail.com>
2026-08-14 11:59:35 -0400

    :sparkles: Starting a system  of the supermarket with POO.

---
+++ POO/Ex_02.py
@@ -0,0 +1,25 @@
+# Stock system of the supermarket:
+
+estoque = {
+    "Arroz": {"quantidade": 45, "preco": 7.89},
+    "Feijão": {"quantidade": 30, "preco": 8.50},
+    "Óleo": {"quantidade": 60, "preco": 6.20},
+    "Açúcar": {"quantidade": 40, "preco": 4.15},
+    "Café": {"quantidade": 25, "preco": 16.90},
+    "Leite": {"quantidade": 120, "preco": 5.49},
+    "Macarrão": {"quantidade": 50, "preco": 3.75},
+    "Molho": {"quantidade": 80, "preco": 2.29},
+    "Sal": {"quantidade": 15, "preco": 2.10},
+    "Farinha": {"quantidade": 35, "preco": 5.30},
+    "Manteiga": {"quantidade": 22, "preco": 11.40},
+    "Queijo": {"quantidade": 18, "preco": 9.98},
+    "Frango": {"quantidade": 14, "preco": 18.90},
+    "Ovos": {"quantidade": 28, "preco": 10.50},
+    "Detergente": {"quantidade": 75, "preco": 2.45},
+    "Sabão": {"quantidade": 20, "preco": 14.99},
+    "Creme": {"quantidade": 45, "preco": 4.20},
+    "Papel": {"quantidade": 32, "preco": 17.80},
+    "Biscoito": {"quantidade": 90, "preco": 3.15},
+    "Suco": {"quantidade": 16, "preco": 12.60}
+}
+
