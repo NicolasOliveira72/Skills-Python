f3458ce4fb2b32be9ea7768e9ce4c6a3dafa9334
NicolasOliveira72 <nicolas1421rocha@gmail.com>
2026-08-13 22:52:02 -0400

    :sparkles: Working with a static method.

--- POO/Ex_01.py
+++ POO/Ex_01.py
@@ -5,6 +5,11 @@ class Car:
     def ligar(self):
         print("Carro ligado!")
 
+    @staticmethod
+    def ascender_farol():
+        print("farois ligados")
+
 carro1 = Car()
 
 carro1.ligar()
+carro1.ascender_farol()
